a83e51cd148cb19e7389fe7533fa258f01713e23
vejnar <charles.vejnar@gmail.com>
2020-10-29 11:22:34 -0400

    Split download from parse

--- labxdb/ncbi.py
+++ labxdb/ncbi.py
@@ -30,79 +30,83 @@ def get_samples_infos(srp, url_search='https://eutils.ncbi.nlm.nih.gov/entrez/eu
         else:
             xmlo = rep_get
         # Parse
-        sra_root = ET.parse(xmlo).getroot()
-        # Check study
-        study = sra_root.find('.//STUDY')
-        if (srp == study.attrib['accession'] or srp == study.attrib['alias']) == False:
-            time.sleep(1)
-            continue
-        # Sample
-        sample = {'runs': []}
-        # Ref
-        sra_sample = sra_root.find('.//SAMPLE')
-        if sra_sample is not None:
-            sample['ref'] = sra_sample.attrib['accession']
-            sample['name'] = sra_sample.attrib['alias']
-        # Title
-        sra_title = sra_root.find('.//SAMPLE/TITLE')
-        if sra_title is not None:
-            sample['label'] = sra_title.text
-        elif 'name' in sample:
-            sample['label'] = sample['name']
-        # Layout
-        if sra_root.find('.//DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_LAYOUT/SINGLE') is not None:
-            paired = False
-        elif sra_root.find('.//DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_LAYOUT/PAIRED') is not None:
-            paired = True
-        else:
-            paired = None
-        # Platform
-        sra_illumina = sra_root.find('.//PLATFORM/ILLUMINA/INSTRUMENT_MODEL')
-        sra_pacbio = sra_root.find('.//PLATFORM/PACBIO_SMRT/INSTRUMENT_MODEL')
-        if sra_illumina is not None:
-            platform = sra_illumina.text
-        elif sra_pacbio is not None:
-            platform = sra_pacbio.text
-        else:
-            platform = None
-        # SRP title
-        sra_title = sra_root.find('.//STUDY_TITLE')
-        if sra_title is not None:
-            srp_title = sra_title.text
-        # Attribute(s)
-        sample['attributes'] = [[sra_att.find('./TAG').text, sra_att.find('./VALUE').text] for sra_att in sra_root.iter('SAMPLE_ATTRIBUTE')]
-        # Run(s)
-        for sra_run in sra_root.iter('RUN'):
-            # Platform
-            sra_illumina = sra_run.find('./PLATFORM/ILLUMINA/INSTRUMENT_MODEL')
-            sra_pacbio = sra_root.find('./PLATFORM/PACBIO_SMRT/INSTRUMENT_MODEL')
-            if sra_illumina is not None:
-                platform = sra_illumina.text
-            elif sra_pacbio is not None:
-                platform = sra_pacbio.text
-            # Stats
-            run = {'ref': sra_run.attrib['accession'],
-                   'spots': int(sra_run.attrib['total_spots']),
-                   'paired': paired,
-                   'platform': platform}
-            sra_stat_per_read = {c.get('index'): c.get('count') for c in sra_run.find('./Statistics')}
-            if '0' in sra_stat_per_read:
-                run['nread1'] = int(sra_stat_per_read['0'])
-            if '1' in sra_stat_per_read:
-                run['nread2'] = int(sra_stat_per_read['1'])
-            # URL
-            for sra_file in sra_run.iter('SRAFile'):
-                if sra_file.attrib['cluster'] == 'public' and sra_file.attrib['supertype'] == 'Primary ETL':
-                    run['sra_url'] = sra_file.attrib['url']
-                    run['sra_size'] = int(sra_file.attrib['size'])
-                    run['sra_md5'] = sra_file.attrib['md5']
-            # Append run
-            if len(import_runs) == 0 or (len(import_runs) > 0 and run['ref'] in import_runs):
-                sample['runs'].append(run)
-        if len(sample['runs']) > 0:
-            sample['runs'].sort(key=lambda x: x['ref'])
+        sample = parse_sra_xml(xmlo, srp)
+        if sample is not None and len(sample['runs']) > 0:
             samples.append(sample)
         # Sleep to reduce request rate
         time.sleep(1)
     samples.sort(key=lambda x: x['ref'])
     return srp_title, samples
+
+def parse_sra_xml(f, srp=None, import_runs=[]):
+    sra_root = ET.parse(f).getroot()
+    # Sample
+    sample = {'runs': []}
+    # Check study
+    study = sra_root.find('.//STUDY')
+    if srp is not None and (srp == study.attrib['accession'] or srp == study.attrib['alias']) == False:
+        return sample
+    # Ref
+    sra_sample = sra_root.find('.//SAMPLE')
+    if sra_sample is not None:
+        sample['ref'] = sra_sample.attrib['accession']
+        sample['name'] = sra_sample.attrib['alias']
+    # Title
+    sra_title = sra_root.find('.//SAMPLE/TITLE')
+    if sra_title is not None:
+        sample['label'] = sra_title.text
+    elif 'name' in sample:
+        sample['label'] = sample['name']
+    # Layout
+    if sra_root.find('.//DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_LAYOUT/SINGLE') is not None:
+        paired = False
+    elif sra_root.find('.//DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_LAYOUT/PAIRED') is not None:
+        paired = True
+    else:
+        paired = None
+    # Platform
+    sra_illumina = sra_root.find('.//PLATFORM/ILLUMINA/INSTRUMENT_MODEL')
+    sra_pacbio = sra_root.find('.//PLATFORM/PACBIO_SMRT/INSTRUMENT_MODEL')
+    if sra_illumina is not None:
+        platform = sra_illumina.text
+    elif sra_pacbio is not None:
+        platform = sra_pacbio.text
+    else:
+        platform = None
+    # SRP title
+    sra_title = sra_root.find('.//STUDY_TITLE')
+    if sra_title is not None:
+        srp_title = sra_title.text
+    # Attribute(s)
+    sample['attributes'] = [[sra_att.find('./TAG').text, sra_att.find('./VALUE').text] for sra_att in sra_root.iter('SAMPLE_ATTRIBUTE')]
+    # Run(s)
+    for sra_run in sra_root.iter('RUN'):
+        # Platform
+        sra_illumina = sra_run.find('./PLATFORM/ILLUMINA/INSTRUMENT_MODEL')
+        sra_pacbio = sra_root.find('./PLATFORM/PACBIO_SMRT/INSTRUMENT_MODEL')
+        if sra_illumina is not None:
+            platform = sra_illumina.text
+        elif sra_pacbio is not None:
+            platform = sra_pacbio.text
+        # Stats
+        run = {'ref': sra_run.attrib['accession'],
+               'spots': int(sra_run.attrib['total_spots']),
+               'paired': paired,
+               'platform': platform}
+        sra_stat_per_read = {c.get('index'): c.get('count') for c in sra_run.find('./Statistics')}
+        if '0' in sra_stat_per_read:
+            run['nread1'] = int(sra_stat_per_read['0'])
+        if '1' in sra_stat_per_read:
+            run['nread2'] = int(sra_stat_per_read['1'])
+        # URL
+        for sra_file in sra_run.iter('SRAFile'):
+            if sra_file.attrib['cluster'] == 'public' and sra_file.attrib['supertype'] == 'Primary ETL':
+                run['sra_url'] = sra_file.attrib['url']
+                run['sra_size'] = int(sra_file.attrib['size'])
+                run['sra_md5'] = sra_file.attrib['md5']
+        # Append run
+        if len(import_runs) == 0 or (len(import_runs) > 0 and run['ref'] in import_runs):
+            sample['runs'].append(run)
+    if len(sample['runs']) > 0:
+        sample['runs'].sort(key=lambda x: x['ref'])
+    return sample
