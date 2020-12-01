c51acae47f74cad1cb4f9efd7c1a39ef2f95822a
vejnar <charles.vejnar@gmail.com>
2020-12-01 16:41:55 -0500

    Add overwrite option. Use f-string

--- script/export_sra_fastq.py
+++ script/export_sra_fastq.py
@@ -24,7 +24,7 @@ def rezip(path_in, fname_out):
     print(fname_out)
     subprocess.run(['zstd', '-dkf', path_in, '-o', fname_out], check=True)
     time.sleep(10)
-    subprocess.run(['gzip', '-9', fname_out], check=True)
+    subprocess.run(['gzip', '-f9', fname_out], check=True)
 
 def main(argv=None):
     if argv is None:
@@ -34,6 +34,7 @@ def main(argv=None):
     parser.add_argument('-s', '--path_seq_run', dest='path_seq_run', action='store', help='Path to run root')
     parser.add_argument('-d', '--path_data', dest='path_data', action='store', default='data.json', help='Run data')
     parser.add_argument('-c', '--path_config', dest='path_config', action='store', help='Path to config')
+    parser.add_argument('-f', '--overwrite', dest='overwrite', action='store_true', help='Overwrite already exported files')
     parser.add_argument('-p', '--processor', dest='num_processor', action='store', type=int, default=2, help='Number of processor')
     args = parser.parse_args(argv[1:])
 
@@ -68,21 +69,23 @@ def main(argv=None):
     jobs = []
     for record in data:
         if record['type'] == 'single':
-            gin = glob.glob(os.path.join(config['path_seq_run'], record['run_ref'], '*%s.zst'%record['pattern']))
+            gin = glob.glob(os.path.join(config['path_seq_run'], record['run_ref'], f"*{record['pattern']}.zst"))
             if len(gin) == 1:
                 path_in = gin[0]
             else:
                 raise ValueError('More than one file found')
-            fname_out = '%s%s'%(record['run_ref'], record['pattern'])
-            jobs.append([path_in, fname_out])
+            fname_out = f"{record['run_ref']}{record['pattern']}"
+            if config['overwrite'] or (config['overwrite'] is False and os.path.exists(fname_out+'.gz') is False):
+                jobs.append([path_in, fname_out])
         elif record['type'] == 'multiplex':
-            gin = glob.glob(os.path.join(config['path_seq_raw'], record['flowcell'], '%s*%s.zst'%(record['tube_label'], record['pattern'])))
+            gin = glob.glob(os.path.join(config['path_seq_raw'], record['flowcell'], f"{record['tube_label']}*{record['pattern']}.zst"))
             if len(gin) == 1:
                 path_in = gin[0]
             else:
                 raise ValueError('More than one file found')
-            fname_out = '%s_%s%s'%(record['flowcell'], record['tube_label'], record['pattern'])
-            jobs.append([path_in, fname_out])
+            fname_out = f"{record['flowcell']}_{record['tube_label']}{record['pattern']}"
+            if config['overwrite'] or (config['overwrite'] is False and os.path.exists(fname_out+'.gz') is False):
+                jobs.append([path_in, fname_out])
 
     # Run jobs
     pfu.parallel.run(rezip, jobs, config['num_processor'])
